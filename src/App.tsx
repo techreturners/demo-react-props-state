87f0fdddc9edaafa2272521c8945071fc6695f14
Ellie Judge <judge_ellie@hotmail.com>
2023-01-24 17:59:27 +0000

    Deletes header element from App.tsx and App.css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
-         <h1>Welcome!</h1>
-        {/* <User name='Gareth' age={35} /> */}
-      </header>
+      <h1>Welcome!</h1>
+      {/* <User name='Gareth' age={35} />  ); */}
     </div>
   );
 }

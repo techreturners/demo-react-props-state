d8f1dc164400ac60c479ee041019458cbcb7dc32
neil-hughes <neil.hughes@techreturners.com>
2023-10-19 09:31:24 +0100

    feat: Add empty task components

---
+++ src/components/task.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Task } from './task_list';
+
+export const TaskDisplay: React.FC<{ task: Task }> = ({ task }) => {
+	return <></>;
+};
---
+++ src/components/task_list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export type Task = {
+	id: string;
+	description: string;
+	done: boolean;
+};
+
+export const TaskList: React.FC = () => {
+	return <></>;
+};
